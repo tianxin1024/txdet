0a92587b11daef280e6c65a843e9def9158f767f
tianxin <tianxin964@126.com>
2022-02-09 22:17:36 +0800

    add tests

---
+++ tests/test_models/test_forward.py
@@ -0,0 +1,62 @@
+import copy
+from os.path import dirname, exists, join, abspath
+
+import sys
+sys.path.append("/home/tianxin/2022/myself/txdet/")
+
+print(sys.path)
+
+from models import my_resnet
+
+import ipdb
+
+
+
+def _get_config_directory():
+    """Find the predefined detector config directory."""
+    try:
+        # Assume we are running in the source mmdetection repo
+        repo_dpath = dirname(dirname(dirname(abspath(__file__))))
+    except NameError:
+        # For IPython development when this __file__ is not defined
+        import mmdet
+        repo_dpath = dirname(dirname(mmdet.__file__))
+    config_dpath = join(repo_dpath, 'configs')
+    if not exists(config_dpath):
+        raise Exception('Cannot find config path')
+    return config_dpath
+
+
+def _get_config_module(fname):
+    """Load a configuration as a python module."""
+    from mmcv import Config
+    config_dpath = _get_config_directory()
+    config_fpath = join(config_dpath, fname)
+    config_mod = Config.fromfile(config_fpath)
+    return config_mod
+
+
+def _get_detector_cfg(fname):
+    """Grab configs necessary to create a detector.
+
+    These are deep copied to allow for safe modification of parameters without
+    influencing other tests.
+    """
+    config = _get_config_module(fname)
+    model = copy.deepcopy(config.model)
+    return model
+
+
+def test_models_forward():
+    model = _get_detector_cfg("/home/tianxin/2022/myself/txdet/configs/my_rpn/my_rpn_r50_fpn_coco.py")
+    # ipdb.set_trace()
+    print(model)
+
+    from mmdet.models import build_detector
+    detector = build_detector(model)
+
+    print(detector)
+
+
+if __name__ == "__main__":
+    test_models_forward()
